8d257b2d52756edd5447e0e70ba4aeeab7076c3d
robertoantonmartinlasalle <roberantn@gmail.com>
2026-04-20 14:02:39 +0200

    feat: añadir endpoint de refresh token para autenticación JWT

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 
 from authentication.views.auth_view import LoginView
-
+from rest_framework_simplejwt.views import TokenRefreshView
 
 urlpatterns = [
     path("login/", LoginView.as_view(), name="login"),
+
+    # REFRESH TOKEN
+    path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
 ]
